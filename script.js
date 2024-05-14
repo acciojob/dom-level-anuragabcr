d933c90630b1f9fff1ce9b53e9b8afdcadc2aaa6
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-14 12:25:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.onload = function() {
+    var element = document.getElementById("level");
+    var domLevel = 0;
+
+    // Traverse through parent elements until reaching the root HTML element
+    while (element.parentElement) {
+        domLevel++;
+        element = element.parentElement;
+    }
+
+    // Display the DOM level using alert
+    alert("The level of the element is: " + domLevel);
+};
